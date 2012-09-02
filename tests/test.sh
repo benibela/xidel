35e7a051cfda4686663cd597dd9dee02c076566d
benibela <benito@benibela.de>
2012-09-03 01:12:20 +0200

    correct quote escaping in tests
    --HG--
    branch : trunks

--- tests/test.sh
+++ tests/test.sh
@@ -3,12 +3,12 @@ mydir=`dirname "$0"`
 
 if [ -f $mydir/$1 ]; then
 
-  if ($mydir/../xidel ${*:2} 2> /dev/null  | diff - $mydir/$1); 
+  if ($mydir/../xidel "${@:2}" 2> /dev/null  | diff - $mydir/$1); 
   then echo OK: $1;
   else echo FAILED: $1
   fi
 else
   echo Missing test case: $1
   echo Creating:
-  $mydir/../xidel ${*:2} | tee $mydir/$1  
+  $mydir/../xidel "${@:2}" | tee $mydir/$1  
 fi
