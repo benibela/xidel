e3e516d73d28cb734442d2c570e0f8e8b3affae4
benibela <benito@benibela.de>
2012-09-03 00:54:42 +0200

    tests
    --HG--
    branch : trunks

--- tests/test.sh
+++ tests/test.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 mydir=`dirname "$0"`
 
-if ($mydir/../xidel ${*:2} 2> /dev/null  | diff - $mydir/$1); 
-then echo SAME: $1;
-else echo DIFF: $1
+if [ -f $mydir/$1 ]; then
+
+  if ($mydir/../xidel ${*:2} 2> /dev/null  | diff - $mydir/$1); 
+  then echo OK: $1;
+  else echo FAILED: $1
+  fi
+else
+  echo Missing test case: $1
+  echo Creating:
+  $mydir/../xidel ${*:2} | tee $mydir/$1  
 fi
--- tests/tests.sh
+++ tests/tests.sh
@@ -6,3 +6,36 @@ tests/test.sh tfe  tests/a.xml -f //a     -e //title
 #Print all titles in all files that can be followed to
 tests/test.sh tef  tests/a.xml -e //title -f //a
 tests/test.sh tefe tests/a.xml -e //title -f //a      -e //title
+
+tests/test.sh direct '<a>foobar</a>' -e '<a>{.}</a>'
+tests/test.sh direct_hidden_vars1 --hide-variable-names '<a>foobar</a>' -e '<a>{.}</a>' 
+tests/test.sh direct_hidden_vars2 '<a>foobar</a>' --hide-variable-names -e '<a>{.}</a>' 
+tests/test.sh direct_hidden_vars3 '<a>foobar</a>' -e '<a>{.}</a>' --hide-variable-names
+
+#read title from both
+tests/test.sh 2urls tests/a.xml -e //title tests/b.xml -e //title 
+#not separated urls
+tests/test.sh 2urls2read tests/a.xml tests/b.xml -e //title -e //title
+
+#stdin
+echo '<test>123<x/>foo<abc>bar</abc>def<x/></test>' | tests/test.sh stdin1 - -e //abc
+echo //abc2 | tests/test.sh stdin2 '<test>123<x/>foo<abc2>bar2!</abc2>def<x/></test>' -e -
+
+#output formats
+tests/test.sh adhoc1 tests/a.xml --extract "<a>{.}</a>*" 
+tests/test.sh xml1 tests/a.xml --extract "<a>{.}</a>*" --output-format xml
+tests/test.sh json1 tests/a.xml --extract "<a>{.}</a>*" --output-format json 
+tests/test.sh xml1b tests/a.xml --output-format xml --extract "<a>{.}</a>*" 
+tests/test.sh json1b tests/a.xml --output-format json --extract "<a>{.}</a>*" 
+
+tests/test.sh adhoc2 tests/a.xml tests/b.xml -e "<a>{.}</a>*"
+tests/test.sh xml2 tests/a.xml tests/b.xml -e "<a>{.}</a>*" --output-format xml
+tests/test.sh json2 tests/a.xml tests/b.xml -e "<a>{.}</a>*" --output-format json 
+tests/test.sh xml2b tests/a.xml tests/b.xml --output-format xml -e "<a>{.}</a>*" 
+tests/test.sh json2b tests/a.xml tests/b.xml --output-format json -e "<a>{.}</a>*" 
+
+tests/test.sh adhoc3 tests/a.xml tests/b.xml --extract "<title>{title:=.}</title><a>{.}</a>*"  
+tests/test.sh xml3 tests/a.xml tests/b.xml --extract "<title>{title:=.}</title><a>{.}</a>*" --output-format xml
+tests/test.sh json3 tests/a.xml tests/b.xml --extract "<title>{title:=.}</title><a>{.}</a>*" --output-format json 
+
+
