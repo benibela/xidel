9fa41299125d0922d4f7796a42624fd22ac30d11
benibela <benito@benibela.de>
2012-12-18 18:50:52 +0100

    clearer test layout
    --HG--
    branch : trunks
    rename : tests/2urls => tests/output/2urls
    rename : tests/2urls2read => tests/output/2urls2read
    rename : tests/adhoc1 => tests/output/adhoc1
    rename : tests/adhoc2 => tests/output/adhoc2
    rename : tests/adhoc3 => tests/output/adhoc3
    rename : tests/direct => tests/output/direct
    rename : tests/direct_hidden_vars1 => tests/output/direct_hidden_vars1
    rename : tests/direct_hidden_vars2 => tests/output/direct_hidden_vars2
    rename : tests/direct_hidden_vars3 => tests/output/direct_hidden_vars3
    rename : tests/google => tests/output/google
    rename : tests/json1 => tests/output/json1
    rename : tests/json1b => tests/output/json1b
    rename : tests/json2 => tests/output/json2
    rename : tests/json2b => tests/output/json2b
    rename : tests/json3 => tests/output/json3
    rename : tests/multipage => tests/output/multipage
    rename : tests/multipage2 => tests/output/multipage2
    rename : tests/regression_entity1a => tests/output/regression_entity1a
    rename : tests/regression_entity1b => tests/output/regression_entity1b
    rename : tests/regression_entity1c => tests/output/regression_entity1c
    rename : tests/regression_entity2 => tests/output/regression_entity2
    rename : tests/regression_entity3a => tests/output/regression_entity3a
    rename : tests/regression_entity3b => tests/output/regression_entity3b
    rename : tests/regression_entity3c => tests/output/regression_entity3c
    rename : tests/regression_entity3d => tests/output/regression_entity3d
    rename : tests/regression_text1a => tests/output/regression_text1a
    rename : tests/regression_text1b => tests/output/regression_text1b
    rename : tests/regression_text1c => tests/output/regression_text1c
    rename : tests/regression_text1d => tests/output/regression_text1d
    rename : tests/regression_text1e => tests/output/regression_text1e
    rename : tests/stdin1 => tests/output/stdin1
    rename : tests/stdin2 => tests/output/stdin2
    rename : tests/t1 => tests/output/t1
    rename : tests/te => tests/output/te
    rename : tests/tef => tests/output/tef
    rename : tests/tefe => tests/output/tefe
    rename : tests/tfe => tests/output/tfe
    rename : tests/xml1 => tests/output/xml1
    rename : tests/xml1b => tests/output/xml1b
    rename : tests/xml2 => tests/output/xml2
    rename : tests/xml2b => tests/output/xml2b
    rename : tests/xml3 => tests/output/xml3

--- tests/test.sh
+++ tests/test.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 mydir=`dirname "$0"`
 
-if [ -f $mydir/$1 ]; then
+if [ -f $mydir/output/$1 ]; then
 
-  if ($mydir/../xidel "${@:2}" 2> /dev/null  | diff - $mydir/$1); 
+  if ($mydir/../xidel "${@:2}" 2> /dev/null  | diff - $mydir/output/$1); 
   then echo OK: $1;
   else echo FAILED: $1
   fi
 else
   echo Missing test case: $1
   echo Creating:
-  $mydir/../xidel "${@:2}" | tee $mydir/$1  
+  $mydir/../xidel "${@:2}" | tee $mydir/output/$1  
 fi
