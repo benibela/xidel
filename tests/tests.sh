fbb3b1653400a5ad2d664416188b2a2cca1de40e
benibela <benito@benibela.de>
2014-03-21 20:02:22 +0100

    fix tests
    --HG--
    branch : trunks

--- tests/tests.sh
+++ tests/tests.sh
@@ -375,6 +375,6 @@ tests/test.sh regression_object2 --dot-notation=on -e '$x := {"b": "c"}' -e '$x.
 tests/test.sh regression_multipage1  -e "<action><page url=\"tests/a.xml\"><template><title>{concat(., \"'\", '\"')}</title></template></page></action>" --extract-kind=multipage
 tests/test.sh regression_multipage2 -e "<action><page url=\"tests/a.xml\"><template><title><t:read var=\"res\" source=\" concat(., &quot;'&quot;, '&quot;')\"/></title></template></page></action>" --extract-kind=multipage
 tests/test.sh regression_multipage3 -e '<action><page url="http://example.org"><template><title>{resolve-uri("b.xml")}</title></template></page></action>' --extract-kind=multipage
-tests/test.sh regression_multipage3b -e '<action><page url="http://example.org/abc/def/ghi"><template><title>{resolve-uri("../b.xml")}</title></template></page></action>' --extract-kind=multipage
+tests/test.sh regression_multipage3b -e '<action><page url="http://videlibri.sourceforge.net/test/reddit/index.html"><template><head>{resolve-uri("../b.xml")}</head></template></page></action>' --extract-kind=multipage
 
 tests/test.sh regression_htmlparse '<ol><li>a<li>b<li>c</ol>' -e '/ol/li'
