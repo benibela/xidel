ade58359dcb6de01eb01200beaeb194e7b04e80b
benibela <benito@benibela.de>
2014-04-21 01:01:34 +0200

    manage scripts for scraper script and xidel download table
    --HG--
    branch : trunks

--- manage.sh
+++ manage.sh
@@ -60,8 +60,8 @@ cgi)    lazCompileLinux64 xidelcgi
 
 release)
         ./manage.sh src
-        ./manage.sh linux64
         ./manage.sh linux32
+        ./manage.sh linux64        
         ./manage.sh win32
         ;;
         
@@ -78,6 +78,24 @@ src)
         fileUpload xidel-$VERSION.src.tar.gz "/Xidel/Xidel\ $VERSION/"
 	;;	
 		
+
+downloadTable)
+
+   xidel http://sourceforge.net/projects/videlibri/files/Xidel/Xidel%20$VERSION/  --extract-kind=xquery  -e 'concat(x"The following Xidel downloads are available on the <a href=&quot;{$url}&quot;>sourceforge download page</a>: <br><br>")' -e 'declare function verboseName($n){ concat ( if (contains($n, "win")) then "Windows: " else if (contains($n, "linux")) then "Universal Linux: " else if (contains($n, ".deb")) then "Debian: " else if (contains($n, "src")) then "Source:" else "", if (contains($n, "32") or contains($n, "386")) then "32 Bit" else if (contains($n, "64"))then "64 Bit" else ""  )   };            
+                           <table class="downloadTable">
+                           <tr><th>Operating System</th><th>Filename</th><th>Size</th></tr>
+                           { for $link in match(<TABLE id="files_list"><t:loop><TR class="file"><TH><A class="name">{{link := object(), link.verboseName := verboseName(.), link.a := .}}</A></TH><td/><td>{{link.size := .}}</td></TR></t:loop></TABLE>, /).link 
+                             order by $link.verboseName descending 
+                             return <tr><td>{$link.verboseName}</td><td><a href="{$link.a/@href}">{$link.a/text()}</a></td><td>{$link.size/text()}</td></tr>}
+                           <tr><td>Mac 10.8</td><td colspan="2"><a href="https://www.evernote.com/shard/s69/sh/ff1e78f3-a369-4855-b18f-6184ce789c45/f3511927d0fb356ce883835f2eb712e0">externally prebuilt version</a> and compile instructions.</td></tr>
+                           </table>'     --printed-node-format xml > /tmp/downloadTable;
+  
+  cat /tmp/downloadTable
+                                                    
+   
+
+  ;;
+
 *)
         echo "Unknown command (use web hg cgi win32 linux32 linux64 src)"
         ;;
