72bb7277af6a0463270cd0290a25fe0a898ed22b
benibela <benito@benibela.de>
2018-04-02 17:01:48 +0200

    fix Travis?

--- meta/build.deb.sh
+++ meta/build.deb.sh
@@ -8,7 +8,7 @@ gzip -9 -n -c meta/debian/changelog > $builddir/usr/share/doc/xidel/changelog.gz
 gzip -9 -n -c meta/xidel.1 > $builddir/usr/share/man/man1/xidel.1.gz
 cp meta/debian/copyright $builddir/usr/share/doc/xidel/copyright
 
-version=$(xidel --version | head -1 | grep -oE "[0-9.]+")
+version=$(./xidel --version | head -1 | grep -oE "[0-9.]+")
 sed -Ee "s/Version:.*/Version: $version/" -i $builddir/DEBIAN/control
 if file xidel | grep x86-64; then arch=amd64; 
 else if file xidel | grep ARM; then arch=arm; else arch=i386; 
