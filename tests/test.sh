ba47b915c1cd85e8ca06a2eece8f2fb39502ea2d
benibela <benito@benibela.de>
2021-12-11 16:10:40 +0100

    colorize test results

--- tests/test.sh
+++ tests/test.sh
@@ -6,17 +6,17 @@ if [ -z "$XIDEL" ]; then XIDEL=$mydir/../xidel; fi
 if [ -f $mydir/output/$1 ]; then
 
   if ($XIDEL "${@:2}" 2> /dev/null  | diff --strip-trailing-cr  $mydir/output/$1 -); 
-  then echo OK: $1;
+  then echo "[1;32mOK[0m": $1;
        echo $1 >> /tmp/xidel-tests-state-ok
-  else echo FAILED: $1
+  else echo '[1;31mFAILED:[0m' $1
    echo $1 >> /tmp/xidel-tests-state-failed
   
    echo "${@:2}" 
   
-   echo "Old (left):"
+   echo "[1;32mOld (left):[0m"
    cat $mydir/output/$1
   
-   echo "New (right):"
+   echo '[1;31mNew (right):[0m'
    $XIDEL "${@:2}" 2> /dev/null
    
    echo -----------------------------------------
