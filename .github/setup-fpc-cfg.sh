5a7da75e3bba316ea155e788cbf6358e2e743437
benibela <benito@benibela.de>
2022-05-08 18:11:32 +0200

    gh actions...

---
+++ .github/setup-fpc-cfg.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo -Fu$PWD/import/flre/src/             >> ~/.fpc.cfg
+echo -Fu$PWD/import/pasdblstrutils/src/   >> ~/.fpc.cfg
+echo -Fu$PWD/import/synapse/              >> ~/.fpc.cfg
+echo -Fi$PWD/internettools/               >> ~/.fpc.cfg
+echo -Fu$PWD/internettools/               >> ~/.fpc.cfg
+echo -Fu$PWD/rcmdline/                    >> ~/.fpc.cfg
+
